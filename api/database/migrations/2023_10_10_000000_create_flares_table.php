4058f2cd65dc123c448ecdead5664b21ee10ac50
trephy <trephyyy@gmail.com>
2025-02-22 14:54:41 +0200

    renamed cme to flare

---
+++ api/database/migrations/2023_10_10_000000_create_flares_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCmesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('flares', function (Blueprint $table) {
+            $table->id();
+            $table->string('flr_id')->unique();
+            $table->string('catalog');
+            $table->string('instrument')->nullable();
+            $table->dateTime('begin_time');
+            $table->dateTime('peak_time');
+            $table->dateTime('end_time');
+            $table->string('class_type')->nullable();
+            $table->string('source_location')->nullable();
+            $table->integer('active_region_num')->nullable();
+            $table->string('note')->nullable();
+            $table->dateTime('submission_time')->nullable();
+            $table->integer('version_id')->nullable();
+            $table->string('link')->nullable();
+            $table->integer('intensity')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cmes');
+    }
+}
