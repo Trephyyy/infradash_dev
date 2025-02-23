b3a9172b3968e14d401a49ceb9dfc04f8fe42663
trephy <trephyyy@gmail.com>
2025-02-23 07:51:02 +0200

    asdasdsa

--- infradash/app/dashboard/page.js
+++ infradash/app/dashboard/page.js
@@ -62,7 +62,6 @@ export default function Dashboard() {
       }));
       console.log('Fetched future data:', processedData);
       setFutureData(processedData);
-      setWarnings(data.warnings); // Assuming warnings are handled
     } catch (error) {
       console.error("Error fetching future predictions:", error);
     }
