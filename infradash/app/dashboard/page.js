123d9c53afa1b5c39e21d4b38f612d3d9ac8020f
trephy <trephyyy@gmail.com>
2025-02-23 09:07:11 +0200

    Adjust Dashboard component heights for improved layout consistency

--- infradash/app/dashboard/page.js
+++ infradash/app/dashboard/page.js
@@ -119,7 +119,7 @@ export default function Dashboard() {
 
           <div className="grid grid-cols-1 md:grid-cols-[1fr_auto_1fr] gap-0 w-full max-w-screen-xl mt-8 items-center">
             <motion.div 
-              className="h-[700px] flex justify-end w-full md:w-[45%]" // Adjusted width
+              className="h-[300px] flex justify-end w-full md:w-[100%]" // Adjusted width
               initial={{ scale: 0.95, opacity: 0 }} 
               animate={{ scale: 1, opacity: 1 }} 
               transition={{ delay: 0.3 }}
@@ -150,7 +150,7 @@ export default function Dashboard() {
             </div>
 
             <motion.div 
-              className="h-[700px] flex justify-start w-full md:w-[90%]" // Adjusted width
+              className="h-[300px] flex justify-start w-full md:w-[100%]" // Adjusted width
               initial={{ scale: 0.95, opacity: 0 }} 
               animate={{ scale: 1, opacity: 1 }} 
               transition={{ delay: 0.4 }}
